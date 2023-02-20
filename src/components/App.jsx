ce831f75afdfea5ce760e8de3a163607827de995
Vitulya <vitulya.bondarchuk@gmail.com>
2023-02-20 13:01:01 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,84 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+const LOCAL_KEY = 'contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem(LOCAL_KEY);
+    const parseContacts = JSON.parse(localContacts);
+    if (parseContacts) {
+      this.setState({
+        contacts: parseContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(LOCAL_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleAddContact = ({ name, number, id }) => {
+    const contact = { id, name, number };
+
+    this.state.contacts.find(contact => contact.name === name)
+      ? alert(`${contact.name} is already in contacts`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, contact],
+        }));
+  };
+
+  changeFilter = value => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  removeContact = id => {
+    this.setState(({ contacts }) => {
+      return {
+        contacts: contacts.filter(item => item.id !== id),
+      };
+    });
+  };
+
+  render() {
+    const { contacts, name, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <>
+        <Section title="Phonebook">
+          <ContactForm handleAddContact={this.handleAddContact} />
+        </Section>
+
+        <Section title="Contacts">
+          <Filter filter={filter} changeFilter={this.changeFilter} />
+          <ContactList
+            filteredContacts={filteredContacts}
+            name={name}
+            removeContact={this.removeContact}
+          />
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+import css from './ContactForm.module.css'
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+
+  handleChangeInput = event => {
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.handleAddContact({
+      name: this.state.name,
+      number: this.state.number,
+      id: nanoid(),
+    });
+
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  
+  render() {
+    return (
+      <form className={css.wrapper} onSubmit={this.handleSubmit}>
+        <label  htmlFor="name">
+          Name
+          <input
+          className={css.input}
+            value={this.state.name}
+            onChange={this.handleChangeInput}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label htmlFor="name">
+          Number
+          <input
+          className={css.input}
+            value={this.state.number}
+            onChange={this.handleChangeInput}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={css.button}>Add form</button>
+      </form>
+    );
+  }
+}
